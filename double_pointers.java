4f0f06e06dc3962b907ccfebee417120c1f1d754
yjy <1304184093@qq.com>
2025-03-13 20:41:19 +0800

    加入双指针

--- double_pointers.java
+++ double_pointers.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class double_pointers {
 //    力扣27. 移除元素
 //    给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
@@ -75,4 +77,44 @@ public class double_pointers {
         nums[left] = nums[right];
         nums[right] = temp;
     }
+    //力扣844. 比较含退格的字符串
+    //给定 s 和 t 两个字符串，当它们分别被输入到空白的文本编辑器后，如果两者相等，返回 true 。# 代表退格字符。
+    //注意：如果对空文本输入退格字符，文本继续为空。
+    public boolean backspaceCompare(String s, String t) {
+        return newString(s).equals(newString(t));
+    }
+    public String newString(String s){
+        char[] s1=s.toCharArray();
+        int slow=0;
+        for (int fast=0;fast<s.length();fast++){
+            if(s1[fast]!='#'){
+                s1[slow]=s1[fast];
+                slow++;
+            }else if (slow>0){
+                slow--;
+            }
+        }
+//        注意这里要用new String，否则返回的是地址，并且要写从0到slow，因为slow指向的是最后一个字符的下一个位置
+        return new String(s1,0,slow);
+    }
+
+    //力扣977. 有序数组的平方
+    //给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+    public int[] sortedSquares(int[] nums) {
+        int left=0,right=nums.length-1;
+        int[] result=new int[nums.length];
+        while (left<=right){
+            for (int k= nums.length-1;k>=0;k--){
+                if (nums[left]*nums[left]<=nums[right]*nums[right]){
+                    result[k]=nums[right]*nums[right];
+                    right--;
+                }else {
+                    result[k]=nums[left]*nums[left];
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+
 }
